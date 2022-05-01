996313a4ca78c2fe829da3ea68d7f2b968ac0a9a
Steve Choi <stevech071@gmail.com>
2022-04-30 19:25:21 -0700

    dashboard css

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -3,14 +3,33 @@ import LogoutButton from '../auth/LogoutButton';
 
 const Dashboard = () => {
     return (
-        <div className='dashboard-wrapper'>
-            <LogoutButton />
-            <div className='dashboard'>
-                <div className='content-box-wrapper'>
-                    
-                </div>
-                <div className='watchlist-wrapper'>
 
+        <div className='dashboard-wrapper'>
+            
+            <div className='dashboard'>
+                <div className='dashboard-title-bar'>
+                    <LogoutButton />
+                </div>
+                <div className='dashboard-content-box'>
+                    <div className='dashboard-content-navbar'>
+
+                    </div>
+                    <div className='dashboard-content'>
+                        <div className='dashboard-content-box1'>
+                            <div>Wallet</div>
+                            <div>Asset</div>
+                            <div>Recent Transaction</div>
+                        </div>
+                        <div className='dashboard-content-box2'>
+                            <div>Graph</div>
+                        </div>
+                        <div className='dashboard-content-box3'>
+                            <div>Portfolio</div>
+                        </div>
+                    </div>
+                    <div className='dashboard-watchlist-box'>
+
+                    </div>
                 </div>
             </div>
         </div>
