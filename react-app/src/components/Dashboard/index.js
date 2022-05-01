7cf0535f804537c21135952db63a5a9ce14ed067
Steve Choi <stevech071@gmail.com>
2022-05-01 13:08:38 -0700

    Dashboard css

--- react-app/src/components/Dashboard/index.js
+++ react-app/src/components/Dashboard/index.js
@@ -1,14 +1,25 @@
 import './index.css'
 import LogoutButton from '../auth/LogoutButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
+
+    const user = useSelector(state => state.session.user)
+
     return (
 
         <div className='dashboard-wrapper'>
-            
+
             <div className='dashboard'>
                 <div className='dashboard-title-bar'>
-                    <LogoutButton />
+                    <div className='profile-icon-box-wrapper'>
+                        <div className='profile-icon-box'>
+                            <FontAwesomeIcon icon={faUser} className='profile-icon' />
+                        </div>
+                    </div>
+                    <h2 className='dashboard-username'>{user?.username}</h2>
                 </div>
                 <div className='dashboard-content-box'>
                     <div className='dashboard-content-navbar'>
@@ -16,15 +27,21 @@ const Dashboard = () => {
                     </div>
                     <div className='dashboard-content'>
                         <div className='dashboard-content-box1'>
-                            <div>Wallet</div>
-                            <div>Asset</div>
-                            <div>Recent Transaction</div>
+                            <div className='wallet-box'>
+                                <h2>Wallet</h2>
+                                <div>
+                                    <p>$</p><p>{user?.wallet.amount}</p>
+                                </div>
+                                <div className='add-button'><FontAwesomeIcon icon={faArrowUp} className='add-icon' /></div>
+                            </div>
+                            <div className='asset-box'><h2>Assets</h2></div>
+                            <div className='recent-box'><h2>Recent</h2></div>
                         </div>
                         <div className='dashboard-content-box2'>
-                            <div>Graph</div>
+                            <div></div>
                         </div>
                         <div className='dashboard-content-box3'>
-                            <div>Portfolio</div>
+                            <div></div>
                         </div>
                     </div>
                     <div className='dashboard-watchlist-box'>
