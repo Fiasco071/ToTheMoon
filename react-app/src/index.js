0ff8137b8a8833045adfef40fcde2618e07603a5
Rami Martinez <40069890+rammartinez00@users.noreply.github.com>
2022-04-30 18:13:00 -0600

    login and signup modals

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
-import configureStore from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+import { ModalProvider } from "./context/Modal";
 
 const store = configureStore();
 
+function Root() {
+  return (
+    <Provider store={store}>
+      <ModalProvider>
+        <App />
+      </ModalProvider>
+    </Provider>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-        <App />
-      </Provider>
+    <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
