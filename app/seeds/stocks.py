16934351f67421415ce3231d6650573193dc774c
Steve Choi <stevech071@gmail.com>
2022-05-04 08:14:10 -0700

    steves nightly work

--- app/seeds/stocks.py
+++ app/seeds/stocks.py
@@ -3,15 +3,15 @@ from app.models import db, Stock
 
 # Adds a demo user, you can add other users here if you want
 def seed_stocks():
-    stock1 = Stock(ticker='RQM666', long_name='Rami Martinez SWAG', i_price=42069.69)
+    stock1 = Stock(ticker='RQM666', long_name='Rami Martinez SWAG', i_price=49.69)
     stock2 = Stock(ticker='KJHK', long_name='BUSSIN BUSSIN CORP', i_price=39.27)
-    stock3 = Stock(ticker='MUKH', long_name='POOGLE', i_price=49.69)
+    stock3 = Stock(ticker='MUKH', long_name='POOGLE', i_price=4.19)
     stock4 = Stock(ticker='FSGF', long_name='APP ACADEMY', i_price=.69)
     stock5 = Stock(ticker='AKJBA', long_name='BLAMAZON', i_price=2.07)
     stock6 = Stock(ticker='KSJHD', long_name='YAHOO', i_price=123.46)
     stock7 = Stock(ticker='78EF', long_name='PIZZA HUTT INTERNATIONAL', i_price=78.87)
-    stock8 = Stock(ticker='DFCJ', long_name='POKEMON', i_price=99999.99)
-    stock9 = Stock(ticker='BVCF', long_name='TACO BELL', i_price=1234.56)
+    stock8 = Stock(ticker='DFCJ', long_name='POKEMON', i_price=12.99)
+    stock9 = Stock(ticker='BVCF', long_name='TACO BELL', i_price=224.56)
     
     
     db.session.add_all([stock1, stock2, stock3, stock4, stock5, stock6, stock7, stock8, stock9])
--- react-app/src/components/Dashboard/PieChart.js
+++ react-app/src/components/Dashboard/PieChart.js
@@ -1,19 +1,19 @@
 import React, { PureComponent } from "react";
-import { PieChart, Pie, ResponsiveContainer, LabelList } from "recharts";
+import { useSelector } from "react-redux";
+import { PieChart, Pie, ResponsiveContainer, Cell, LabelList } from "recharts";
 //Lab, Sector, Cell removed from recharts import to take away console warnings
-const data = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-  { name: "Group E", value: 278 },
-  { name: "Group F", value: 189 },
-];
 
-export default class AssetChart extends PureComponent {
-  static demoUrl = "https://codesandbox.io/s/pie-chart-of-straight-angle-oz0th";
 
-  render() {
+const AssetChart = () => {
+  const data = []
+  const assets = useSelector(state => state.assets)
+  Object.values(assets).forEach(asset => {
+    let value = (+asset.stock.i_price * +asset.num_shares)
+    let obj = { "name": asset.stock.ticker, "value": value  }
+    data.push(obj)
+  }) 
+  const COLORS = ['#FF8042', '#00C49F', '#FFBB28', '#0088FE' ];
+
     return (
       <ResponsiveContainer width="100%" height="100%">
         <PieChart width={400} height={400}>
@@ -21,16 +21,24 @@ export default class AssetChart extends PureComponent {
             dataKey="value"
             startAngle={180}
             endAngle={0}
+            innerRadius={0}
+            paddingAngle={2}
             data={data}
             cx="50%"
             cy="50%"
-            outerRadius={70}
+            outerRadius={80}
             fill="#FB881D"
             label
-          />
+          >
+{data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+            </Pie>
+
           <LabelList dataKey="name" position="insideTop" />
         </PieChart>
       </ResponsiveContainer>
     );
-  }
 }
+
+export default AssetChart
