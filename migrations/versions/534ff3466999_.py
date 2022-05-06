3e1f5e97f37a70f93ada0068319c5b6c4fb06e1e
Kristen Reid <kreiddetroit@gmail.com>
2022-05-06 13:41:07 -0400

    added to asset route

--- migrations/versions/534ff3466999_.py
+++ migrations/versions/534ff3466999_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 23c3527711e6
+Revision ID: 534ff3466999
 Revises: 
-Create Date: 2022-05-05 18:22:17.637070
+Create Date: 2022-05-05 20:47:15.176620
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '23c3527711e6'
+revision = '534ff3466999'
 down_revision = None
 branch_labels = None
 depends_on = None
